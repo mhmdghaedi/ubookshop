afe4297dff308f87d946eed40404e953d9f7e13f
mhmdghaedi <$mhmd.ghaedi890@gmail.com>
2024-09-01 22:16:23 +0330

    Update

--- pages/api/testt.ts
+++ pages/api/testt.ts
@@ -6,7 +6,7 @@ import type { NextApiRequest, NextApiResponse } from "next"
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   let body = JSON.parse(req.body)
-  console.log(body)
+
 
   res.send({ code: 0})
 }
